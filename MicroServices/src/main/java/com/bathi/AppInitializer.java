6564076b8d6834856a20419491af5f909a5a91cb
Srikanth Bathi <bathi.srikanth@sap.com>
2018-04-23 14:22:55 +0530

    First push
    Change-Id: I0389e1a3c9737534144fa1c447d1a56ed0eb8471
    Signed-off-by: Srikanth Bathi <bathi.srikanth@sap.com>

---
+++ MicroServices/src/main/java/com/bathi/AppInitializer.java
@@ -0,0 +1,31 @@
+package com.bathi;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements  WebApplicationInitializer  {
+
+	
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+
+		//get the application context
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(ContextInitializer.class);
+		
+		DispatcherServlet servlet = new DispatcherServlet(appContext);
+		servletContext.addServlet("dispatcher", servlet).addMapping("/");
+		
+		servletContext.addListener(new ContextLoaderListener());
+		
+		
+		
+	}
+
+}
---
+++ MicroServices/src/main/java/com/bathi/controllers/Advertisement.java
@@ -0,0 +1,5 @@
+package com.bathi.controllers;
+
+public class Advertisement {
+
+}
