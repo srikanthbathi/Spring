3ce035377a4c90aef846271f90baa776b9313c15
Srikanth Bathi <bathi.srikanth@sap.com>
2018-04-24 14:57:21 +0530

    Add @RequestParam -- URL = ? id=123 /@PathVariable
    Change-Id: Ic862e460edfe55f3adac2e64a468a60828420826
    Signed-off-by: Srikanth Bathi <bathi.srikanth@sap.com>

--- MicroServices/src/main/java/com/bathi/controllers/Advertisement.java
+++ MicroServices/src/main/java/com/bathi/controllers/Advertisement.java
@@ -1,16 +1,48 @@
 package com.bathi.controllers;
 
+import java.util.HashMap;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bathi.payloads.Advertisements;
 
 @RestController
 @RequestMapping("/hello")
 public class Advertisement {
 
-	@GetMapping
-	public String getDefault() {
-		return "OK!";
+	private HashMap<Integer, Advertisements> adds = new HashMap<Integer, Advertisements>();
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public HashMap<Integer, Advertisements> getAllAdds() {
+		return adds;
 	}
+	
+	@GetMapping("/{id}")
+	public Advertisements getAdd(@PathVariable("id") Integer id){
+		return adds.get(id);
+	}
+	
+	
+	@PostMapping
+	public Integer addAdv(@RequestBody Advertisements add){
+		adds.put(adds.size(), add);
+		return adds.size()-1;
+		
+	}
+	
+	@PutMapping
+	public Integer deleteAdd(@RequestParam("id") Integer id){
+		adds.remove(id);
+		return id; 
+	}
+	
 	
 }
---
+++ MicroServices/src/main/java/com/bathi/payloads/Advertisements.java
@@ -0,0 +1,14 @@
+package com.bathi.payloads;
+
+public class Advertisements {
+ private String title;
+
+public String getTitle() {
+	return title;
+}
+
+public void setTitle(String title) {
+	this.title = title;
+}
+ 
+}
