7ac3d37aa54e50daedb78930392ab4efc8e40f42
Srikanth Bathi <bathi.srikanth@sap.com>
2018-04-26 10:47:13 +0530

    test
    Change-Id: I9d0ac7c1ba561ac74da756eb5a5d588421f5ff27
    Signed-off-by: Srikanth Bathi <bathi.srikanth@sap.com>

---
+++ MicroServices/src/test/java/com/bathi/controllers/AdvertisementTest.java
@@ -0,0 +1,56 @@
+package com.bathi.controllers;
+
+import static org.junit.Assert.fail;
+
+import javax.inject.Inject;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import com.bathi.ContextInitializer;
+import com.bathi.payloads.Advertisements;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {ContextInitializer.class})
+@WebAppConfiguration
+public class AdvertisementTest {
+	
+	@Inject
+	WebApplicationContext context;
+	
+	private MockMvc mockmvc;
+	
+	@Before
+	public void setUp() throws Exception{
+		mockmvc = MockMvcBuilders.webAppContextSetup(context).build();
+		
+	}
+	
+	private MockHttpServletRequestBuilder buildpostRequest(String title) throws JsonProcessingException{
+		Advertisements add = new Advertisements();
+		add.setTitle(title);
+		return buildpostRequest("/hello").content(new ObjectMapper().writeValueAsString(add)).
+				contentType(MediaType.APPLICATION_JSON_VALUE);
+	}
+	
+	@Test
+	public void createAdd() throws JsonProcessingException, Exception{
+		mockmvc.perform(buildpostRequest("title")).
+		andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isCreated());
+		
+	}
+	
+	
+	
+}
